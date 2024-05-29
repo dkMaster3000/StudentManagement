91762a98cc606559c93609eb73b9f753bc6e4135
dkMaster3000 <kunzd007@gmail.com>
2024-05-29 16:21:30 +0200

    added student config

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,8 +3,6 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
@@ -20,18 +18,5 @@ public class StudentService {
     public List<Student> getStudents() {
 
         return studentRepository.findAll();
-
-/*        return List.of(
-                new Student(1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.APRIL, 5),
-                        21),
-                new Student(1L,
-                        "John",
-                        "jon.jamal@gmail.com",
-                        LocalDate.of(2001, Month.APRIL, 5),
-                        22)
-        );*/
     }
 }
